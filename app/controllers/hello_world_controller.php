8d490b5fa927a7c4ba9e3721e1bbafb4e2f3d618
Peltomäki Saara K <pelsaara@da5-cs-b221-21.pc.helsinki.fi>
2017-03-22 16:43:36 +0200

    Ensimmäiset html-näkymät valmiit

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,32 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        //View::make('home.html');
+        echo 'Tämä on etusivu!';
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        View::make('helloworld.html');
     }
-  }
+
+    public static function tuote_list() {
+        View::make('suunnitelmat/game_list.html');
+    }
+
+    public static function tuote_show() {
+        View::make('suunnitelmat/game_show.html');
+    }
+    
+    public static function tuote_edit() {
+        View::make('suunnitelmat/game_edit.html');
+    }
+
+    public static function login() {
+        View::make('suunnitelmat/login.html');
+    }
+
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,24 @@
 <?php
 
-  $routes->get('/', function() {
+$routes->get('/', function() {
     HelloWorldController::index();
-  });
+});
 
-  $routes->get('/hiekkalaatikko', function() {
+$routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
-  });
+});
+
+$routes->get('/tuote', function() {
+    HelloWorldController::tuote_list();
+});
+$routes->get('/tuote/1', function() {
+    HelloWorldController::tuote_show();
+});
+
+$routes->get('/login', function() {
+    HelloWorldController::login();
+});
+
+$routes->get('/tuote/1/edit', function() {
+    HelloWorldController::tuote_edit();
+});
