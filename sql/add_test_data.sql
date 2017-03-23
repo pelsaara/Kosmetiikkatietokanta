3f0406de209498dc96f31b9cff02201562b25e4e
Peltomäki Saara K <pelsaara@da3-cs-dk110-05.pc.helsinki.fi>
2017-03-23 13:12:09 +0200

    Tietokantataulut luotu ja testidataa syötetty

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,27 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Consumer (name, password, age) VALUES ('Minni', 'Minni123', 19);
+INSERT INTO Consumer (name, password, age) VALUES ('Lilli', 'Lilli123', 29);
+
+INSERT INTO Brand (name) VALUES ('Too Faced');
+INSERT INTO Brand (name) VALUES ('Benefit');
+INSERT INTO Brand (name) VALUES ('Lumene');
+
+INSERT INTO Product (name, brand, description, ingredients) VALUES ('Better Than Sex Mascara', 1, 'Tuuheuttava ja pidentävä ripsiväri', 'Aqua, Beeswax, Ink');
+INSERT INTO Product (name, brand, description, ingredients) VALUES ('True Mystic Volume Mascara', 3, 'Tuuheuttava ja pidentävä ripsiväri', 'Aqua, Beeswax, Ink');
+INSERT INTO Product (name, brand, description, ingredients) VALUES ('Gimme Brow!', 2, 'Tuuheuttava kuitukarvoja sisältävä värillinen kulmageeli', 'Aqua, Beeswax');
+
+INSERT INTO Category (name) VALUES ('Värikosmetiikka');
+INSERT INTO Category (name) VALUES ('Ihonhoito');
+INSERT INTO Category (name) VALUES ('Kulmakarvat');
+INSERT INTO Category (name) VALUES ('Ripsivärit');
+
+INSERT INTO ProductCategory (product, category) VALUES (1, 1);
+INSERT INTO ProductCategory (product, category) VALUES (1, 4);
+INSERT INTO ProductCategory (product, category) VALUES (2, 1);
+INSERT INTO ProductCategory (product, category) VALUES (2, 4);
+INSERT INTO ProductCategory (product, category) VALUES (3, 1);
+INSERT INTO ProductCategory (product, category) VALUES (3, 3);
+
+INSERT INTO Comment (product, consumer, comment) VALUES (1, 1, 'Paras ripsari!');
+INSERT INTO Comment (product, consumer, comment) VALUES (1, 3, 'Luottotuote :)');
+INSERT INTO Comment (product, consumer, comment) VALUES (2, 2, 'Kotimainen loistotuote!!');
+
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,37 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Consumer(
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  password varchar(50) NOT NULL,
+  age INTEGER NOT NULL
+);
+
+CREATE TABLE Brand(
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL
+);
+  
+CREATE TABLE Product(
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  brand INTEGER REFERENCES Brand(id),  
+  description varchar(500),
+  ingredients varchar(500)
+);
+
+CREATE TABLE Category(
+  id SERIAL PRIMARY KEY, 
+  name varchar(50) NOT NULL
+);
+
+CREATE TABLE ProductCategory(
+  product INTEGER REFERENCES Product(id),
+  category INTEGER REFERENCES Category(id)
+);
+
+CREATE TABLE Comment(
+  id SERIAL PRIMARY KEY, 
+  product INTEGER REFERENCES Product(id),
+  consumer INTEGER REFERENCES Consumer(id),
+  comment varchar(500) NOT NULL
+);
+
